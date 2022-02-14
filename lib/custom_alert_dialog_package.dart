ca46e9991255be7994660a414520b6a887025123
Oscar Anyiam <oscaranyiam94@gmail.com>
2022-02-14 14:54:05 +0100

    second commit

--- lib/custom_alert_dialog_package.dart
+++ lib/custom_alert_dialog_package.dart
@@ -1,7 +1,38 @@
 library custom_alert_dialog_package;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// A Custom alert Dialog
+class CustomAlertDialog {
+  final String? title;
+  final String? message;
+
+  CustomAlertDialog({required this.title, required this.message});
+
+  static Future showAlertDialog(
+      {required BuildContext context, required Widget displayWidget}) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 20,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: const Text("Alert Dialog"),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                displayWidget,
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Ok"),
+                  color: Colors.blue,
+                )
+              ],
+            ),
+          );
+        });
+  }
 }
